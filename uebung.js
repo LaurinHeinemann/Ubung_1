54cd2129f3aff9275eb870a29e1584543371919d
Laurin Heinemann <heinemla@r02-pc09.SCHULE>
2022-02-10 11:17:10 +0100

    Initial commit

---
+++ uebung.js
@@ -0,0 +1,18 @@
+let beliebigerString = "keks";
+let eineZahl = 17;
+let nochEineZahl = 7;
+let ergebnis;
+let eineZahlMitKomma = 200;
+
+function rechenspiele() {
+    ergebnis = eineZahl * nochEineZahl;
+    if (ergebnis > eineZahlMitKomma) {
+        console.log (ergebnis - eineZahlMitKomma)
+    }
+    else {
+        console.log ("Das Ergebnis ist zu klein")
+    }
+}
+function ungerade() {
+    for (let i = "Ende")
+}
